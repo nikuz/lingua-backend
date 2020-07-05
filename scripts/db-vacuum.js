1ca381cf764d5669a8186bbdfd6e920d22725aa7
nikuz <micurino@gmail.com>
2020-07-05 12:00:32 +0200

    fix DB file path in db vacuum script

--- scripts/db-vacuum.js
+++ scripts/db-vacuum.js
@@ -1,7 +1,7 @@
 //
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database(path.resolve(__dirname, 'dictionary.SQLITE3'));
+let db = new sqlite3.Database(path.resolve(__dirname, '../database/dictionary.SQLITE3'));
 
 db.serialize(() => {
     db.run(`VACUUM;`);
